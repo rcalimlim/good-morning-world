5bbc9db505ec19b0457249aa1ede7b9d06b57641
Ross Calimlim <ross.calimlim@gmail.com>
2019-06-29 19:45:59 -0500

    init

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Get request successful!');
+});
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
