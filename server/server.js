60d4d4ba7d3b6b3008bdd0da20bb599a9456f3d9
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-02 21:52:19 -0500

    Require dotenv for env vars

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./database/database');
 const app = express();
 const port = 3000;
+
 
 app.use(cors());
 app.use(express.json());
